40e485f795c200e890a726cd05c82c74c687b541
awooooool <45804108+awooooool@users.noreply.github.com>
2023-11-18 14:48:11 +0700

    feat(Routing): add routing and internet gateways

--- main.tf
+++ main.tf
@@ -15,14 +15,62 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_eip" "IP" {
+  domain = "vpc"
+}
+
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_nat_gateway" "NAT" {
+  subnet_id     = aws_subnet.Public.id
+  allocation_id = aws_eip.IP.id
+  depends_on    = [aws_subnet.Public, aws_eip.IP]
+}
+
+resource "aws_subnet" "Public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+}
+
 resource "aws_subnet" "Private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.10.0/24"
 }
 
+resource "aws_route_table" "Public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+}
+
+resource "aws_route_table" "Private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.NAT.id
+  }
+}
+
+resource "aws_route_table_association" "Public" {
+  subnet_id      = aws_subnet.Public.id
+  route_table_id = aws_route_table.Public.id
+}
+
+resource "aws_route_table_association" "Private" {
+  subnet_id      = aws_subnet.Private.id
+  route_table_id = aws_route_table.Private.id
+}
+
 resource "aws_security_group" "sg" {
   name        = "Allow SSH and HTTP 8080"
   description = "Allow SSH and HTTP 8080 from anywhere"
+  vpc_id      = aws_vpc.main.id
 
   ingress {
     from_port   = 22
@@ -64,12 +112,13 @@ data "local_file" "user_data" {
 }
 
 resource "aws_instance" "FastAPI" {
-  ami           = data.aws_ami.Amazon-Linux-2023.id
-  instance_type = "t3.micro"
-  count         = 5
-  key_name      = "voclab"
-  user_data     = data.local_file.user_data.content
-  subnet_id     = aws_subnet.Private.id
+  ami                         = data.aws_ami.Amazon-Linux-2023.id
+  instance_type               = "t3.micro"
+  count                       = 5
+  user_data                   = data.local_file.user_data.content
+  subnet_id                   = aws_subnet.Public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.sg.id]
 
   tags = {
     Name      = "FastAPI"
