18e8a8e4d81ddfb529cf18eb9b791a1a65db7ab9
awooooool <45804108+awooooool@users.noreply.github.com>
2023-11-19 22:32:03 +0700

    feat(Subnet): randomize availability zone

--- subnets.tf
+++ subnets.tf
@@ -1,21 +1,28 @@
+resource "random_shuffle" "Subnet-AZ" {
+  input        = data.aws_availability_zones.Subnets-AZ.names
+  result_count = 2
+}
+
 resource "aws_subnet" "Public-A" {
   vpc_id            = aws_vpc.Main.id
   cidr_block        = "10.0.0.0/24"
-  availability_zone = "ap-southeast-1a"
+  availability_zone = random_shuffle.Subnet-AZ.result[0]
 }
 
 resource "aws_subnet" "Public-B" {
   vpc_id            = aws_vpc.Main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1b"
+  availability_zone = random_shuffle.Subnet-AZ.result[1]
 }
 
 resource "aws_subnet" "Private-A" {
-  vpc_id     = aws_vpc.Main.id
-  cidr_block = "10.0.10.0/24"
+  vpc_id            = aws_vpc.Main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = random_shuffle.Subnet-AZ.result[0]
 }
 
 resource "aws_subnet" "Private-B" {
-  vpc_id     = aws_vpc.Main.id
-  cidr_block = "10.0.11.0/24"
+  vpc_id            = aws_vpc.Main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = random_shuffle.Subnet-AZ.result[1]
 }
