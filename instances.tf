1e109b7c7789f4eac4292930dc3c8494929ce675
awooooool <45804108+awooooool@users.noreply.github.com>
2023-11-20 13:48:12 +0700

    feat(Instance): round robin AZ instead of random_shuffle

--- instances.tf
+++ instances.tf
@@ -19,20 +19,12 @@ variable "Instances-Count" {
   type = number
 }
 
-resource "random_shuffle" "Instances-Subnet" {
-  input        = aws_subnet.Private[*].id
-  count        = var.Instances-Count
-  result_count = 1
-}
-
 resource "aws_instance" "FastAPI" {
-  for_each = {
-    for k, az in random_shuffle.Instances-Subnet : k => az
-  }
+  count                       = var.Instances-Count
   ami                         = data.aws_ami.Amazon-Linux-2023.id
   instance_type               = "t3.micro"
   user_data                   = data.local_file.user_data.content
-  subnet_id                   = each.value.result[0]
+  subnet_id                   = aws_subnet.Private[count.index % var.Total-AZ].id
   associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.Workers.id]
   depends_on                  = [aws_nat_gateway.NAT] // To wait for NAT Gateway to active before deploying instance
